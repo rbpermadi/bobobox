56277f95d5cf5bfc4a18c08f81a410b18f21df29
Rojali Budi Permadi <rojali717@gmail.com>
2020-09-09 20:51:30 +0700

    update hotel_repo

--- internal/repository/hotel_repo.go
+++ internal/repository/hotel_repo.go
@@ -13,7 +13,8 @@ type HotelEntity struct {
 }
 
 type IHotelRepo interface {
-	SearchRoomAvailabilities(int, int, string, string, string) []HotelEntity
+	SearchHotelRoomAvailabilities(int, int, string, string, string) []HotelEntity
+	CountHotelRoomAvailabilities(string, string, string) int
 }
 
 type HotelRepo struct {
@@ -25,13 +26,8 @@ func NewHotelRepo(db *sqlx.DB) *HotelRepo {
 	return &HotelRepo{db}
 }
 
-func (hotelRepo *HotelRepo) SearchRoomAvailabilities(limit int, offset int, checkinDate string, checkoutDate string, hotelIds string) []HotelEntity {
-	hotelIdsCondition := ``
+func (hotelRepo *HotelRepo) SearchHotelRoomAvailabilities(limit int, offset int, checkinDate string, checkoutDate string, hotelIdsCondition string) []HotelEntity {
 	he := []HotelEntity{}
-
-	if hotelIds != "" {
-		hotelIdsCondition = " AND h.id IN (" + hotelIds + ") "
-	}
 
 	query := `
 	SELECT 
@@ -77,10 +73,58 @@ func (hotelRepo *HotelRepo) SearchRoomAvailabilities(limit int, offset int, chec
 	`
 
 	err := hotelRepo.db.Select(&he, query, checkinDate, checkoutDate, offset, limit)
-
 	if err != nil {
 		return []HotelEntity{}
 	}
 
 	return he
+}
+
+func (hotelRepo *HotelRepo) CountHotelRoomAvailabilities(checkinDate string, checkoutDate string, hotelIdsCondition string) int {
+	var count int
+
+	query := `
+	SELECT 
+		count(rc.id)
+	FROM
+		(
+			SELECT
+				h.*, COUNT(r.id) as total_rooms
+			FROM
+				hotel h
+			JOIN
+				room r ON h.id = r.hotel_id
+			WHERE
+				r.room_status = 'available' ` + hotelIdsCondition + `
+			GROUP BY
+				h.id
+		) as rc
+	LEFT OUTER JOIN
+		(
+			SELECT id, MAX(guests.total_guest) as max_guest
+			FROM
+				(
+					SELECT 
+						h.id, sr.date, COUNT(sr.id) as total_guest
+					FROM
+						hotel h
+					JOIN
+						room r ON r.hotel_id = h.id
+					JOIN
+						stay_room sr ON r.id = sr.room_id
+					WHERE
+						sr.date BETWEEN ? AND ? ` + hotelIdsCondition + `
+					GROUP BY
+						sr.date, h.id
+				) as guests 
+			GROUP BY id
+		) as rg ON rc.id = rg.id AND rc.total_rooms > rg.max_guest
+	`
+
+	err := hotelRepo.db.Get(&count, query, checkinDate, checkoutDate)
+	if err != nil {
+		return 0
+	}
+
+	return count
 }
